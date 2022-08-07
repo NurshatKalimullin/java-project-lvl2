0ec24ab92ac0702f9acb238d21c49e36692c0783
Nurshat Kalimullin <nurshatkalimullin@mail.ru>
2022-08-07 15:55:14 +0300

    Add filepath parameters

--- src/main/java/hexlet/code/App.java
+++ src/main/java/hexlet/code/App.java
@@ -2,10 +2,21 @@ package hexlet.code;
 
 import picocli.CommandLine;
 
+import java.io.File;
+
 @CommandLine.Command(name = "gendiff", mixinStandardHelpOptions = true, version = "differ 4.0",
         description = "Compares two configuration files and shows a difference.")
 public class App {
 
+
+    @CommandLine.Parameters(index = "0", description = "path to first file", defaultValue = "/etc/hosts", paramLabel = "filepath1")
+    private File filepath1 = new File("/etc/hosts");
+
+    @CommandLine.Parameters(index = "1", description = "path to second file", defaultValue = "/etc/hosts", paramLabel = "filepath2")
+    private File filepath2 = new File("/etc/hosts");
+
+    @CommandLine.Option(names = {"-f", "--format"}, description = "output format [default: stylish]", paramLabel = "format")
+    private String format = "stylish";
 
     public static void main(String[] args) {
         int exitCode = new CommandLine(new App()).execute(args);
