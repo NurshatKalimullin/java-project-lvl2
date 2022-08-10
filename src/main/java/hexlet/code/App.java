0b4abf39ac7546a1ad4933d0d16f5ec8cbb72b98
Nurshat Kalimullin <nurshatkalimullin@mail.ru>
2022-08-10 21:49:02 +0300

    Read from json file

--- src/main/java/hexlet/code/App.java
+++ src/main/java/hexlet/code/App.java
@@ -1,20 +1,24 @@
 package hexlet.code;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import picocli.CommandLine;
 
 import java.io.File;
 import java.nio.file.Files;
+import java.util.Arrays;
+import java.util.Map;
 import java.util.concurrent.Callable;
 
 @CommandLine.Command(name = "gendiff", mixinStandardHelpOptions = true, version = "differ 4.0",
         description = "Compares two configuration files and shows a difference.")
 public class App implements Callable<String> {
 
-    @CommandLine.Parameters(index = "0", description = "path to first file", defaultValue = "/etc/hosts", paramLabel = "filepath1")
-    private File filepath1 = new File("/etc/hosts");
+    @CommandLine.Parameters(index = "0", description = "path to first file", defaultValue = "project2files/file1.json", paramLabel = "filepath1")
+    private File filepath1 = new File("project2files/file1.json");
 
-    @CommandLine.Parameters(index = "1", description = "path to second file", defaultValue = "/etc/hosts", paramLabel = "filepath2")
-    private File filepath2 = new File("/etc/hosts");
+    @CommandLine.Parameters(index = "1", description = "path to second file", defaultValue = "project2files/file2.json", paramLabel = "filepath2")
+    private File filepath2 = new File("project2files/file1.json");
 
     @CommandLine.Option(names = {"-f", "--format"}, description = "output format [default: stylish]", paramLabel = "format")
     private String format = "stylish";
@@ -24,13 +28,32 @@ public class App implements Callable<String> {
     public String call() throws Exception {
         byte[] firstFileContents = Files.readAllBytes(filepath1.toPath());
         byte[] secondFileContents = Files.readAllBytes(filepath2.toPath());
+        String json = "{ \"color\" : \"Black\", \"type\" : \"BMW\" }";
+        if (format.equals("JSON")) {
+            System.out.println("It is JSON");
+            ObjectMapper mapper = new ObjectMapper();
+
+            Map<String, Object> firstMap
+                    = mapper.readValue(firstFileContents, new TypeReference<Map<String,Object>>(){});
+            for (String keys : firstMap.keySet())
+            {
+                System.out.println(keys);
+            }
+
+            Map<String, Object> secondMap
+                    = mapper.readValue(secondFileContents, new TypeReference<Map<String,Object>>(){});
+            for (String keys : secondMap.keySet())
+            {
+                System.out.println(keys);
+            }
+        }
         return "";
     }
 
 
     public static void main(String [] args) {
-        //int exitCode = new CommandLine(new CheckSum()).execute(args);
-        //System.exit(exitCode);
+        int exitCode = new CommandLine(new App()).execute(args);
+        System.exit(exitCode);
         System.out.println("Hello World!");
     }
 
