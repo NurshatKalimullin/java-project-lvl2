67e4ed36d31cced8799bff8e64778c290dc02d06
Nurshat Kalimullin <nurshatkalimullin@mail.ru>
2022-08-13 22:42:31 +0300

    Compare and sort

--- src/main/java/hexlet/code/App.java
+++ src/main/java/hexlet/code/App.java
@@ -6,9 +6,10 @@ import picocli.CommandLine;
 
 import java.io.File;
 import java.nio.file.Files;
-import java.util.Arrays;
-import java.util.Map;
+import java.util.*;
+import java.util.Comparator;
 import java.util.concurrent.Callable;
+import java.util.stream.Collectors;
 
 @CommandLine.Command(name = "gendiff", mixinStandardHelpOptions = true, version = "differ 4.0",
         description = "Compares two configuration files and shows a difference.")
@@ -35,17 +36,71 @@ public class App implements Callable<String> {
 
             Map<String, Object> firstMap
                     = mapper.readValue(firstFileContents, new TypeReference<Map<String,Object>>(){});
-            for (String keys : firstMap.keySet())
+//            for (String keys : firstMap.keySet())
+//            {
+//                System.out.println(keys);
+//            }
+
+            Map<String, Object> secondMap
+                    = mapper.readValue(secondFileContents, new TypeReference<Map<String,Object>>(){});
+//            for (String keys : secondMap.keySet())
+//            {
+//                System.out.println(keys);
+//            }
+
+
+            Map<String, Object> difference
+                    = new HashMap<>();
+
+
+            for (Map.Entry<String, Object> one :
+                    firstMap.entrySet()) {
+                if (!secondMap.containsKey(one.getKey())) {
+                    difference.put("- " + one.getKey(), one.getValue());
+                } else if (!secondMap.get(one.getKey()).equals(one.getValue())) {
+                    difference.put("- " + one.getKey(), one.getValue());
+                } else {
+                    difference.put("  " + one.getKey(), one.getValue());
+                }
+            }
+            for (Map.Entry<String, Object> two :
+                    secondMap.entrySet()) {
+                if (!firstMap.containsKey(two.getKey())) {
+                    difference.put("+ " + two.getKey(), two.getValue());
+                } else if (!firstMap.get(two.getKey()).equals(two.getValue())) {
+                    difference.put("+ " + two.getKey(), two.getValue());
+                } else {
+                    difference.put("  " + two.getKey(), two.getValue());
+                }
+            }
+
+            List<String> newList = new ArrayList<>(difference.keySet());
+
+
+            List<String> elList = newList
+                    .stream()
+                    .sorted((o1, o2) ->
+                            o1.substring(2).compareToIgnoreCase(o2.substring(2))
+                    )
+                    .collect(Collectors.toList());
+
+            LinkedHashMap<String, Object> last
+                    = new LinkedHashMap<>();
+
+            for (String e : elList) {
+                for (Map.Entry<String, Object> o :
+                        difference.entrySet()) {
+                    if (e.equals(o.getKey())) {
+                        last.put(e, o.getValue());
+                    }
+                }
+            }
+
+            for (String keys : last.keySet())
             {
                 System.out.println(keys);
             }
 
-            Map<String, Object> secondMap
-                    = mapper.readValue(secondFileContents, new TypeReference<Map<String,Object>>(){});
-            for (String keys : secondMap.keySet())
-            {
-                System.out.println(keys);
-            }
         }
         return "";
     }
