95cc80b2ab350debc765eb00f853183fe8968a88
Nurshat Kalimullin <nurshatkalimullin@mail.ru>
2022-08-21 10:31:42 +0300

    Add plain test

--- src/main/java/hexlet/code/formats/Plain.java
+++ src/main/java/hexlet/code/formats/Plain.java
@@ -14,7 +14,6 @@ public class Plain {
                 newMap.put(entry.getKey(), entry.getValue());
             }
         }
-        System.out.println(newMap);
         for (Map.Entry<String, Object> entry1 : newMap.entrySet()) {
             String key1 = entry1.getKey();
 
@@ -26,63 +25,38 @@ public class Plain {
                     Object value2 = entry2.getValue();
 
                     if (key1.substring(2).equals(key2.substring(2)) &&
-                            entry2.getKey().startsWith("+ ")) {
+                            key2.startsWith("+ ")) {
                         result = result + "Property '"
-                                + entry1.getKey().substring(2)
+                                + key1.substring(2)
                                 + "' was updated. From "
                                 + formatValue(value1)
                                 + " to "
                                 + formatValue(value2)
                                 + "\n";
+                        break;
                     }
                 }
+                if (!newMap.containsKey("+ " + key1.substring(2))){
+                    result = result + "Property '"
+                            + key1.substring(2)
+                            + "' was removed"
+                            + "\n";
+
+                }
+
+            } else if (key1.startsWith("+ ") && !newMap.containsKey("- " + key1.substring(2))){
+                result = result + "Property '"
+                        + key1.substring(2)
+                        + "' was added with value: "
+                        + formatValue(entry1.getValue())
+                        + "\n";
 
             }
+
         }
-        System.out.println(newMap);
         System.out.println(result);
-        return "";
-
-//        Property 'chars2' was updated. From [complex value] to false
-//        Property 'checked' was updated. From false to true
-//        Property 'default' was updated. From null to [complex value]
-//        Property 'id' was updated. From 45 to null
-//        Property 'key1' was removed
-//        Property 'key2' was added with value: 'value2'
-//        Property 'numbers2' was updated. From [complex value] to [complex value]
-//        Property 'numbers3' was removed
-//        Property 'numbers4' was added with value: [complex value]
-//        Property 'obj1' was added with value: [complex value]
-//        Property 'setting1' was updated. From 'Some value' to 'Another value'
-//        Property 'setting2' was updated. From 200 to 300
-//        Property 'setting3' was updated. From true to 'none'
-
-
-//        {
-//            chars1: [a, b, c]
-//            - chars2: [d, e, f]
-//            + chars2: false
-//                - checked: false
-//                + checked: true
-//                - default: null
-//                + default: [value1, value2]
-//            - id: 45
-//                    + id: null
-//                    - key1: value1
-//                    + key2: value2
-//            numbers1: [1, 2, 3, 4]
-//            - numbers2: [2, 3, 4, 5]
-//            + numbers2: [22, 33, 44, 55]
-//            - numbers3: [3, 4, 5]
-//            + numbers4: [4, 5, 6]
-//            + obj1: {nestedKey=value, isNested=true}
-//        - setting1: Some value
-//        + setting1: Another value
-//        - setting2: 200
-//                + setting2: 300
-//                - setting3: true
-//                + setting3: none
-//        }
+        result = result.substring(0, result.length() - 1);
+        return result;
     }
 
     private static String formatValue(Object value) {
@@ -92,7 +66,12 @@ public class Plain {
                 value.getClass().getSimpleName().equals("LinkedHashMap"))) {
             result = "[complex value]";
         } else {
-            result = String.valueOf(value);
+            if (!String.valueOf(value).equals("null") &&
+                    value.getClass().getSimpleName().equals("String")) {
+                result = "'" + String.valueOf(value) + "'";
+            } else {
+                result = String.valueOf(value);
+            }
         }
         return result;
     }
--- src/test/java/hexlet/code/DifferTests.java
+++ src/test/java/hexlet/code/DifferTests.java
@@ -102,4 +102,25 @@ public class DifferTests {
         String diff = Differ.generate(firstFile, secondFile);
         assertThat(diff).isEqualTo(expected);
     }
+
+    @Test
+    public void testPlainResult() throws Exception {
+        String expected = "Property 'chars2' was updated. From [complex value] to false\n"
+            + "Property 'checked' was updated. From false to true\n"
+            + "Property 'default' was updated. From null to [complex value]\n"
+            + "Property 'id' was updated. From 45 to null\n"
+            + "Property 'key1' was removed\n"
+            + "Property 'key2' was added with value: 'value2'\n"
+            + "Property 'numbers2' was updated. From [complex value] to [complex value]\n"
+            + "Property 'numbers3' was removed\n"
+            + "Property 'numbers4' was added with value: [complex value]\n"
+            + "Property 'obj1' was added with value: [complex value]\n"
+            + "Property 'setting1' was updated. From 'Some value' to 'Another value'\n"
+            + "Property 'setting2' was updated. From 200 to 300\n"
+            + "Property 'setting3' was updated. From true to 'none'";
+        String firstFile = "src/test/resources/file1.json";
+        String secondFile = "src/test/resources/file2.json";
+        String diff = Differ.generate(firstFile, secondFile);
+        assertThat(diff).isEqualTo(expected);
+    }
 }
